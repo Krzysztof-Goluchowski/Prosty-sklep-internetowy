bd534960e0039971c7c90715d315f8aebe28f63c
Krzysztof Gołuchowski <kgoluchowski@student.agh.edu.pl>
2023-12-28 00:44:45 +0100

    nice background photo :)

--- react-projekt/src/App.js
+++ react-projekt/src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import NavBar from "./components/NavBar";
 import NavHeader from "./components/NavHeader";
 import Logo from "./components/Logo";
+import FirstBlockHomePage from "./components/FirstBlockHomePage";
+import SecondBlockHomePage from "./components/SecondBlockHomePage";
+import Temporary from "./components/Temporary";
+import ThirdBlockHomePage from "./components/ThirdBlockHomePage";
+import NiceBackgroudImage from "./components/NiceBackgroudImage";
 
 function App() {
   return (
@@ -10,7 +15,11 @@ function App() {
             <Logo/>
             <NavBar/>
         </NavHeader>
-
+        <Temporary/>
+        <FirstBlockHomePage/>
+        <SecondBlockHomePage/>
+        <NiceBackgroudImage/>
+        <ThirdBlockHomePage/>
     </div>
   );
 }
---
+++ react-projekt/src/components/NiceBackgroudImage.js
@@ -0,0 +1,12 @@
+function NiceBackgroundImage() {
+    return (
+        <>
+            <div className="NiceBackgroundImage">
+                <p className="obramowka_tesktu">ALE ESSA</p>
+                <p className="obramowka_tesktu">Pozdrownia do wiezienia</p>
+            </div>
+        </>
+    );
+}
+
+export default NiceBackgroundImage;
