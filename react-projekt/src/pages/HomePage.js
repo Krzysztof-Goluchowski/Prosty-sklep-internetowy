c2a6dd69a33b094f62f2daf0a5056136d24845f1
Krzysztof Gołuchowski <kgoluchowski@student.agh.edu.pl>
2023-12-30 01:42:00 +0100

    Build image slider

--- react-projekt/src/pages/HomePage.js
+++ react-projekt/src/pages/HomePage.js
@@ -8,8 +8,26 @@ import FirstBlockHomePage from "../components/FirstBlockHomePage";
 import SecondBlockHomePage from "../components/SecondBlockHomePage";
 import NiceBackgroudImage from "../components/NiceBackgroudImage";
 import ThirdBlockHomePage from "../components/ThirdBlockHomePage";
+import image_1 from "../photos/image-1.jpg";
+import image_2 from "../photos/image-2.jpg";
+import image_3 from "../photos/image-3.jpg";
+import image_4 from "../photos/image-4.jpg";
+import image_5 from "../photos/image-5.jpg";
 
 function HomePage() {
+    const slides = [
+        {url: image_1, title: "runing man"},
+        {url: image_2, title: "dead lift"},
+        {url: image_3, title: "dead lift girl xD"},
+        {url: image_4, title: "man with ropes"},
+        {url: image_5, title: "our nice receptionists"},
+    ];
+
+    const containerStyles = {
+        width: "100vw",
+        height: "90vh",
+        margin: "0 auto",
+    }
     return (
         <div>
             <NavHeader>
@@ -17,7 +35,9 @@ function HomePage() {
                 <NavBar/>
             </NavHeader>
             <Temporary/>
-            <FirstBlockHomePage/>
+            <div style={containerStyles}>
+                <FirstBlockHomePage slides={slides}/>
+            </div>
             <SecondBlockHomePage/>
             <NiceBackgroudImage/>
             <ThirdBlockHomePage/>
