9b7b60ed8361e6c101faf43d3ec9f40b71bf5f90
Krzysztof Gołuchowski <kgoluchowski@student.agh.edu.pl>
2024-01-14 14:18:44 +0100

    Add Nice background image with quote

--- react-projekt/src/components/NiceBackgroudImage.js
+++ react-projekt/src/components/NiceBackgroudImage.js
@@ -1,9 +1,35 @@
 function NiceBackgroundImage() {
+    const pStyle = {
+        color: 'white',
+        margin: '5px',
+        padding: '0px',
+        fontFamily: 'Courier New, monospace',
+        fontStyle: 'italic',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    };
+
+    const bigTextStyle = {
+        fontWeight: 'bold',
+        fontSize: '75px',
+        marginLeft: '20px',
+        marginRight: '20px'
+    };
+
+    const smallTextStyle = {
+        fontSize: '50px'
+    };
     return (
         <>
             <div className="NiceBackgroundImage">
-                <p className="obramowka_tesktu">ALE ESSA</p>
-                <p className="obramowka_tesktu">Pozdrownia do wiezienia</p>
+                <p style={pStyle}>
+                    <span style={bigTextStyle}>SUKCES  </span> <span style={smallTextStyle}>to suma</span> <span style={bigTextStyle}>MAŁYCH</span>
+                </p>
+                <p style={pStyle}><span style={smallTextStyle}>wysiłków powtarzanych</span></p>
+                <p style={pStyle}>
+                    <span style={bigTextStyle}>DZIEŃ PO DNIU</span>
+                </p>
             </div>
         </>
     );
